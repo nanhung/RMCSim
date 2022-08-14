a6c3d6558dd4f301961097d3631366e3870333ca
Nan-Hung Hsieh <d99622005@ntu.edu.tw>
2022-08-13 18:23:32 -0700

    Update mcsim.R

--- R/mcsim.R
+++ R/mcsim.R
@@ -28,36 +28,35 @@ mcsim <- function(model, input, dir = ".", parallel = F){
   #SetPoints_line <- grep("SetPoints \\(", x=tx)
   
   if (length(MCMC_line) != 0){
-    
     RandomSeed <- sample(1111:9999, 1)
     tx2 <- gsub(pattern = "10101010", replacement = paste(RandomSeed), x = tx)
+  }
+  
+  if (parallel == T){
+    i <- RandomSeed
+    name <- gsub("\\..*", "", input)
+    mcmc_input <- paste0(name, "_", i, ".in")
+    mcmc_output <- paste0(name, "_", i, ".out")
+    tx3 <- gsub(pattern = "MCMC.default.out", replacement = mcmc_output, x = tx2)
+    writeLines(tx3, con = mcmc_input)
+    system(paste("./mcsim.", model, ".exe -i 1000 ", mcmc_input, sep = ""))
     
+    checkfile <- paste0("MCMC.check_", i,".out")
     
-    if (parallel == T){
-      i <- RandomSeed
-      name <- gsub("\\..*", "", input)
-      mcmc_input <- paste0(name, "_", i, ".in")
-      mcmc_output <- paste0(name, "_", i, ".out")
-      tx3 <- gsub(pattern = "MCMC.default.out", replacement = mcmc_output, x = tx2)
-      writeLines(tx3, con = mcmc_input)
-      system(paste("./mcsim.", model, ".exe -i 1000 ", mcmc_input, sep = ""))
-      
-      checkfile <- paste0("MCMC.check_", i,".out")
-      
-      tx4 <- gsub(pattern = ",0,", replacement = ",1,", x = tx3)
-      tx5 <- gsub(pattern = paste0("\"", mcmc_output, "\",\"\""),
-                  replacement = paste0("\"", checkfile, "\",\"", mcmc_output, "\""),
-                  x = tx4)
-      tmp <- paste0("tmp_", mcmc_input)
-      writeLines(tx5, con=paste0(dir, "/", tmp))
-      system(paste("./mcsim.", model, ".exe ", dir, "/", tmp, sep = ""))
-      file.remove(paste0(dir, "/", tmp))
-      
-      if(file.exists(checkfile)){
-        message(paste0("* Create '", checkfile, "' from the last iteration."))
+    tx4 <- gsub(pattern = ",0,", replacement = ",1,", x = tx3)
+    tx5 <- gsub(pattern = paste0("\"", mcmc_output, "\",\"\""),
+                replacement = paste0("\"", checkfile, "\",\"", mcmc_output, "\""),
+                x = tx4)
+    tmp <- paste0("tmp_", mcmc_input)
+    writeLines(tx5, con=paste0(dir, "/", tmp))
+    system(paste("./mcsim.", model, ".exe ", dir, "/", tmp, sep = ""))
+    file.remove(paste0(dir, "/", tmp))
+    
+    if(file.exists(checkfile)){
+      message(paste0("* Create '", checkfile, "' from the last iteration."))
       
       
-    } else{ # single chain test
+    } else{
       checkfile <- "MCMC.check.out"
       tmp <- paste0("tmp_", input)
       writeLines(tx, con=paste0(dir, "/", input))
